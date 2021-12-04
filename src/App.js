a8b797f35be66975016a07091934863aae76eedc
Tommy Nguyen <tommy.nguyen@pc260.na.corp.samsungelectronics.net>
2021-12-03 23:29:11 -0500

    Completed exercises 1 - 3, need to fix exercise 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
+import PerspectiveDecoration from './components/PerspectiveDecoration';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <>
+    
       <CharacterEditor />
+      <PerspectiveDecoration />
       <Footer />
     </>
   );
---
+++ src/components/PerspectiveDecoration/PerspectiveDecoration.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './PerspectiveDecoration.module.css';
+
+const PerspectiveDecoration = () => <div className={styles.perspectiveDecoration}></div>
+
+export default PerspectiveDecoration;
